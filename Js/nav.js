840b8fabeec42c21ea208db14a7c1ed248b4de8b
Vambes <36566249+Vambes@users.noreply.github.com>
2018-03-03 22:58:40 +0300

    ав

---
+++ Js/nav.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Vambes on 21.02.2018.
+ */
+$(function() {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.nav__liner').addClass("nav__liner-fixed");
+        } else {
+            $('.nav__liner').removeClass("nav__liner-fixed");
+        }
+    });
+});
